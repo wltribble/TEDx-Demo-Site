fb57a709a3542b5a914f4171298d4dbbb9b15fbd
Will Tribble <wltribble@gmail.com>
2017-03-21 12:29:14 -0500

    Add files via upload

---
+++ JavaScript/Main.js
@@ -0,0 +1,19 @@
+function navbarToggle() {
+    document.getElementById("navbar").classList.toggle("show");
+    document.getElementById("+").classList.toggle("active");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+        document.getElementById("+").classList.remove("active");
+      }
+    }
+  }
+}
